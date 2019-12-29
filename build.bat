7f82af26edc93cc01dc1beaaf448457e10b9704d
Eugene Dvoretsky <radioxoma@gmail.com>
2019-12-29 16:43:20 +0300

    Cleaning up deploy

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 echo Windows build script invoked
-pyinstaller --onefile --noconsole sandbox\__main__.py --name sandbox
+pyinstaller --onefile --noconsole sandbox/__main__.py --name sandbox
 sleep 1
 dist/sandbox.exe
-export WINDOWS_FILE="dist/sandbox.exe"
+SET WINDOWS_FILE="dist/sandbox.exe"
